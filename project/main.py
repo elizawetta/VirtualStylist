3bad919a998316c5804b07a67e1fb59c4cf5b107
Eilzaveta <eilzaveta@MacBook-Air-Eilzaveta.local>
2024-03-20 11:53:41 +0300

    update registation

---
+++ project/main.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template, g
+from flask_login import login_required, current_user, mixins
+from . import db
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/')
+def index():
+    name = ''
+    if current_user.is_authenticated:
+        name = current_user.login
+    else:
+        name = 'Login'
+    return render_template('index.html', name=name)
+
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.login)
+
+
+@main.route('/favorites')
+@login_required
+def favorites():
+    return render_template('favorites.html', name=current_user.login)
+
+
+@main.route('/search')
+@login_required
+def search():
+    return render_template('search.html', name=current_user.login)
