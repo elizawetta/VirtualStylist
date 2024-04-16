8570797b009061eda0a5d99cf07892e00abe41a3
Eilzaveta <eilzaveta@MacBook-Air-Eilzaveta.local>
2024-04-17 00:53:54 +0300

    update db

--- project/init_db.py
+++ project/init_db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 
 connection = sqlite3.connect('instance/db.sqlite')
 
@@ -7,17 +8,22 @@ connection = sqlite3.connect('instance/db.sqlite')
 #     connection.executescript(f.read())
 
 cur = connection.cursor()
-# connection.executescript('''CREATE TABLE user (
-#     id INTEGER PRIMARY KEY AUTOINCREMENT,
-#     email TEXT NOT NULL,
-#     password TEXT NOT NULL,
-#     login TEXT NOT NULL);''')
-# cur.execute()execute
+connection.executescript('''CREATE TABLE potho (
+                         id INTEGER PRIMARY KEY AUTOINCREMENT,
+                         img_path TEXT NOT NULL,
+                         clothes TEXT NOT NULL,
+                         color_hsv FLOAT NOT NULL,
+                         bl FLOAT NOT NULL,
+                         wh FLOAT NOT NULL,
+                         color TEXT NOT NULL
+                         );''')
 
-# cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-#             ('Second Post', 'Content for the second post')
-#             )
-# print(cur.rowcount)
+path = '../img_proessing/marked_images.csv'
+with open(path) as f:
+    for i in f.readlines()[1:]:
+        i = i.strip().split(';')
+    cur.execute("INSERT INTO potho (img_path, clothes, color_hsv, bl, wh, color) VALUES (?, ?, ?, ?, ?, ?)",
+                (i))
 
 # cur.execute("INSERT INTO user (email, password, login) VALUES (?, ?, ?)",
 #             ('lari.x@mail.ru', 'lol', 'lol')
--- project/models.py
+++ project/models.py
@@ -9,4 +9,23 @@ class User(UserMixin, db.Model):
     login = db.Column(db.String(1000))
 
 
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    img_path = db.Column(db.String(100))
+    clothes = db.Column(db.String(100))
+    color_hsv = db.Column(db.Float)
+    bl = db.Column(db.Float)
+    wh = db.Column(db.Float)
+    color = db.Column(db.String(100))
+
+
+class Interaction():
+    id = db.Column(db.Integer, primary_key=True)
+    img_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    state = db.Column(db.Integer) # 1 - like, 2 - dislike, 0 - not viewed 
+    timestamp = db.Column(db.DateTime, nullable=True)
+
+
+
 
